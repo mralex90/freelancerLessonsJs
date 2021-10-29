391906b6ffd84d791c95793aeebf825145b72170
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-29 22:35:19 +0300

    assigning a value to a variable

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
-let eyeColor = 'blue';
-console.log(eyeColor);
+//  ОБЪЯВЛЕНИЕ ПЕРЕМЕННОЙ 
 
-let user;
-let userName = 'Вася';
+// ключевое слово для обьявления переменной
 
-user = userName;
-console.log(user);
+//  имя переменной может содержатьтолько буквы, цифры и символы $ и _. 
+// При этом первый символ не может быть цифорй
+
+// Верное обьявление:
+let age;
+let info123;
+let $size;
+let _color;
+
+// Неверное  обьявление
+let 123info;
+let my-age;
+
+// Вместо 
+let s;
+let sdkjkldlgsljl;
+let leftsidebarsize;
+
+// Лучше писать 
+let size;
+let age;
+let leftSidebarSize;
+
+//Регистр имеет значение
+let sidebarsize;
+let sidebarSize;
+
+// Нельзя использовать зарезервированые имена
+
+let let;
+let break;
+let for;
+
+
+// ПРИСВОЕНИЕ ЗНАЧЕНИЯ ПЕРЕМЕННОЙ
+
+// Объявление без значения
+// let myLifeStyle;
+
+// Присвоение значения
+// myLifeStyle = 'freelance';
+
+// Объявление переменной и присвоение значения
+let myLifeStyle = 'freelance';
+console.log(myLifeStyle);
+
