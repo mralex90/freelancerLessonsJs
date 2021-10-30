cf3c791a01d3416283e1f5d4504c48dda230a43e
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-30 22:12:31 +0300

    создавать переменные постоянно?

--- js/script.js
+++ js/script.js
@@ -1,69 +1,22 @@
-//    ========= CONST ==========
+// Стоит ли каждый раз создавать новую переменную или, все таки, переназначить обьявленную ранее?
 
-// Обьявление константы
-const myAge = 30;
-myAge = 21; // нельзя изменить константу
+let userName = '';
+// ... некий код с использованием userName...
 
-// Имена констант с заранее известными значениями
-const BLOCK_SIZE = 25;
-const COLOR_RED = "#F00";
+userName = 30;
 
-// Имя константы сл значением присвоенным в процессе выполнения программы
-const dummSizes = 25 + 30;
+//  ... некий код с использованием userName...
 
+userName = 'Kharkiv';
 
-// Смена значений в константе
-// Обьект с данными
-const userProfile = {
-    name: 'Alex',
-    age: 30,
-    address: 'Kharkiv' 
-}
-console.log(userProfile);
+//  ... некий код с использованием userName...
 
-// Далее я меняю значение одного из полей обьекта
-userProfile.age = 21;
-// Ошибки нет, данные изменились
-console.log(userProfile);
-// Тоже самое сработает для массивов и др обьектных значений
+// ???????
 
+// НУЖНО СОЗДАВАТЬ ОТДЕЛЬНУЮ ПЕРЕМЕННУЮ ДЛЯ КАЖДОГО ОТДЕЛЬНОГО ЗНАЧЕНИЯ
 
-// ========= КЛЮЧЕВОЕ СЛОВО VAR =========
+let userName = 'Alex';
+let userAge = 30;
+let userAddress = 'Kharkiv';
 
-// Использование переменной до ее обьявления
-
-// "use strict"
-
-age = 30;
-console.log(age);
-
-var age;
-
-// Области видимости переменной
-
-function testBlockLet() {
-    let myAgeLet = 30;
-}
-// Пременная НЕ видна за пределами блока console.log(myAgeLet);
-
-function testBlockVar() {
-    var myAgeVar = 30;
-}
-
-testBlockVar();
-
-// Переменная не видна заа пределами блока console.log(myAgeVar);
-
-
-
-if (true) {
-    let sizeLet = 50;
-}
-
-// Пременная НЕ видна за пределами блока console.log(sizeLet);
-
-if (true) {
-    var sizeVar = 50;
-}
-
-// Переменная ВИДНА за пределами блока console.log(sizeVar);
+//  ... некий код с использованием userName, userAge, userAddreess...
