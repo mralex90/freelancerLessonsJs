46c94d8c3f50fdcdfa7300f5ec8e722601500243
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-11-02 21:20:02 +0200

    Number ( Infinity, -Infinity, Nan)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
-// Boolean 
+// Number (Число)
 /*
-Boolean - это Булевый или логический тип данных.
-Может принимать только два значения: true (истина) и false (ложь).
+Числовой тип данных представляет значения как в виде целых чисел, 
+так и в виде чисел с плаввющей запятой
 */
-//
-// let willYouMarryMe = true;
-// if(willYouMarryMe) {
-//     console.log(':)');
-// } else {
-//     console.log(':(');
-// }
 
-// Так же можно использовать с операторами сравнения
-let trueOrFalse = 58 < 18;
-console.log(trueOrFalse);
+let userAge = 20;
+let userHeight = 1.83;
+console.log(userAge);
+console.log(typeof userAge);
+console.log(userHeight);
+console.log(typeof userHeight);
+/* Но не только числа может возвращать тип NUmber,
+существуют такие специальные числовые значения, такие как:
+Infinity, -Infinity , NaN
+*/
+
+// Infinity 
+// Это математическа бесконечность, которая больше любго числа
+// let getInfinity = 58 / 0;
+// console.log(getInfinity);
+// console.log(typeof getInfinity);
+
+// -Infinity
+/* 
+let getInfinity = -58 / 0;
+console.log(getInfinity);
+console.log(typeof getInfinity);
+*/
+
+// NaN
+// Означает вычислительную ошибку - результат неправильной либо неопределенной математической операции
+ 
+
+/*
+если где-то в математическом выражении есть NaN, то результатом вычислений с его участием будет NaN.
+*/
