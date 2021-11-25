5e7bb8be2bb49375edf1c18531899ef6f51bb14f
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-11-25 23:27:55 +0200

    Округление чисел

--- js/script.js
+++ js/script.js
@@ -1,24 +1,62 @@
-// Шестнадцатеричные, двоичные и восьмеричные числа
+// Округдение чисел 
+
+// В JS встроен обьект Math, котрый содержит несколько ф-ий для работы с округлением:
+ 
+// Math.floor - Округление в меньшую сторону
+
+let numOne = Math.floor(5.8);
+let numTwo = Math.floor(2.2);
+let numThree = Math.floor(-2.2);
+
+console.log(numOne);
+console.log(numTwo);
+console.log(numThree);
+
+// Math.ceil - Округление в большую сторону
+
+let numOne1 = Math.ceil(5.8);
+let numTwo1 = Math.ceil(2.2);
+let numThree1 = Math.ceil(-2.2);
+
+console.log(numOne1);
+console.log(numTwo1);
+console.log(numThree1);
+
+// Math.round - Округление до ближайшего целого 
+
+let numOne2 = Math.round(5.8);
+let numTwo2 = Math.round(2.2);
+let numThree2 = Math.round(-2.2);
+
+console.log(numOne2);
+console.log(numTwo2);
+console.log(numThree2);
+/*
+Math.round округляет до целого числа, а чтобы округлить до 5.8 или 5.85 есть пара способов:
+1) Способ умножения и деления
+*/
+let num1 = Math.round(5.845 * 10) / 10; // 58.45 -> 58 -> 5.8
+console.log(num1);
+
+let num2 = Math.round(5.845 * 100) / 100; // 584.5 -> 585 -> 5.85
+console.log(num2);
+
+let num3 = Math.round(5.8449 * 100) / 100; // 584.49 -> 584 -> 5.84
+console.log(num3);
 
 /*
-Шестнадцатеричные числа широко используются в ДЖС для представления 
-цветов, кодировки символв и многого другого.
+2) Способ: метод toFixed(n) - округляет число до "n" знаков после запятой 
+и возвращает строковое представление результата. 
+Берем число, присваиваем его в переменную, далее переменной применяем метод 
+"toFixed", в скобках указывая количество знаков после запятой, которые нужно получить.
+В результате получаем строку, в которой написано 5.8.
+
+Чтобы преобразовать эту строку в число - можем использовать либо унарный оператор (+), 
+либо специальную ф-ию "Number".
+В обоих случаях мы получим число 5.8
 */
-
-console.log(0xFF); // 0xFF = 255
-
-// Реже используются двоичные и восьмеричные числа
-
-console.log(0b11111111); // 255
-console.log(0o377); // 255
-
-/*Метод toString(base)
-Метод num.toString(base) возвращает строковое пресдтавление числа 
-в той или иной системе счисления (base).
-base может принимать значения от 2 до 36 ( по умолчанию 10).
-*/
-
-let num = 255;
-console.log(num.toString(16));
-console.log(num.toString(8));
-console.log(num.toString(2));
+let num4 = 5.845;
+console.log(num4.toFixed(1));
+// Преобразить в число
+console.log(+num4.toFixed(1));
+console.log(Number(num4.toFixed(1)));
