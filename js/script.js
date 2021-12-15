9e92736a3dd2cf579abe7e9cde215a4e19ed531e
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-12-15 23:48:14 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,58 +1,32 @@
-// Поиск в массиве 
+//  Сортировка массивов
 
-// find u findIndex
-/* В случае когда у нас массив, точнее его элементы являются обьектами нужно 
-пользоваться этими методами. Эти методы обеспечивают поиск в массиве обьектов 
-с определенным условием и возвращает элемент первый попавшийся который удовлетворит 
-этому условию.
+// Метод sort(fn)
+//сортирует масств на месте, меняя в нем порядок элементов.
+
+// Сортировка слов 
+let arrOne = [ 'Ivan', 'Olya', 'Aleks',];
+console.log(arrOne.sort());
+
+// Сортировка чисел
+let arrTwo = [8, 22, 1,];
+console.log(arrTwo.sort());
+
+// По умолчанию элементы сортируются как строки. Для строк применяется 
+// лексикографический порядок, и действительно выходит, что "8" > "22".
+console.log("8" > "22");
+
+// Фунуция сортировки
+function compareNumeric(a, b) {
+    console.log('Сравниваем ${a} и ${b}');
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+
+    // return a - b
+}
+// console.log(arrTwo.sort((a, b) => a -b));
+
+console.log(arrTwo.sort(compareNumeric));
 
 
-// Синтаксис
-let result = arr.find(function(item, index, array) {
-    // если true - возвращается текущий элемент и перебор прерывется
-    // если все итерации оказались ложными, возвращается undefined
-});
-*/
-
-let arr = [
-    { name: 'Aleks', age: 31},
-    { name: 'Ivan', age: 18},
-    { name: 'Olya', age: 'No comment'},
-]
-
-let resultOne = arr.find(function (item, index, array) {
-    return item.age === 18;
-}); // можно записать это же с помощью стрклочной ф-ии
-
-// let resultOne = arr.find(item => item.age === 18);
-console.log(resultOne);
-
-// findIndex   - возвращает не элемент, а его индекс/ключ
-let resultTwo = arr.findIndex(item => item.age === 18);
-console.log(resultTwo);
-
-//////////
-
-// filter
-/*
-Этот метод очень похож на метод find, но разница в том что он не прекращает 
-свою работу в тот момент, когда удовлетворено указанное условие, а продолжает ее, 
-и соответственно, возвращает массив из всех элементов которые удовлетворяют 
-указанному условию.
-
-Метод ищет все элементы, на которых функция-колбэк вернет true.
-let result = arr.filter(function (item, index, array) {
-    // если true - элемент добавляется к результату, и перебор продолжается
-    // возвращается пустой массив в случае, если ничего не найдено
-});
-*/
-
-let arr1 = [
-    { name: 'Aleks', age: 31},
-    { name: 'Ivan', age: 18},
-    { name: 'Olya', age: 'No comment'},
-]
-let result = arr1.filter(function (item, index, array) {
-    return item.age >= 18;
-});
-console.log(result);
+////// 23:06
