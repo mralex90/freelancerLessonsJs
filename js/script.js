a4e60101ccea3ec0c5ac8880b32c8e04d08da8a8
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-12-16 23:33:11 +0200

    reverse

--- js/script.js
+++ js/script.js
@@ -1,32 +1,11 @@
-//  Сортировка массивов
+// Сортировка массивов
 
-// Метод sort(fn)
-//сортирует масств на месте, меняя в нем порядок элементов.
+// Метод reverse
+// меняет порядок элементов в массиве на обратный.
 
-// Сортировка слов 
-let arrOne = [ 'Ivan', 'Olya', 'Aleks',];
-console.log(arrOne.sort());
-
-// Сортировка чисел
-let arrTwo = [8, 22, 1,];
-console.log(arrTwo.sort());
-
-// По умолчанию элементы сортируются как строки. Для строк применяется 
-// лексикографический порядок, и действительно выходит, что "8" > "22".
-console.log("8" > "22");
-
-// Фунуция сортировки
-function compareNumeric(a, b) {
-    console.log('Сравниваем ${a} и ${b}');
-    if (a > b) return 1;
-    if (a == b) return 0;
-    if (a < b) return -1;
-
-    // return a - b
-}
-// console.log(arrTwo.sort((a, b) => a -b));
-
-console.log(arrTwo.sort(compareNumeric));
+let arrOne = ['Aleks', 'Ivan', 'Olya'];
+console.log(arrOne.reverse());
 
 
-////// 23:06
+/////////
+
