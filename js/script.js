7ec0f23a7129d04e57215317939883021bf657e7
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-01-05 23:13:57 +0200

    Изменение документа (textContent)

--- js/script.js
+++ js/script.js
@@ -2,25 +2,23 @@
 // DOM
 // Изменение документа
 
-// Содержание элемента целиком outerHTML(содержимое + САМ обьект)
+// Просто текст элемента textContent
 
 // Получаем обьект
 const textElement = document.querySelector('.lesson__text');
-
-// Получаем содержимое обьекта "как есть" вместе с HTML, а также сам элемент
-const textElementContent = textElement.outerHTML;
+const textElementContent = textElement.textContent;
 console.log(textElementContent);
-// Помимо самого содержимого (innerHTML) мы получаем еще и сам обьект, его оболочку (в д.с. <div class="lesson__text">Text</div>)
 
-/* Есть еще одна особенность. 
-Попробуем изменить документ. Т.е. обращаемся к нашему элементу, пишем outerHTML и записываем 
-туда абсолютно новый обьект (вместо дива с классом текст пишем просто параграф и некую фразу).
-Видим что документ изменился. Если перейдем  во вкладку ELEMENTS, то в этом убедимся.
-
-Но, если мы заново захотим получить outerHTML данного обьекта - мы все еще получим старую оригинальную запись.
-*/
-
-textElement.outerHTML = `<p>${textElementContent}</p> <p>And 
+// Полезная возможность textContent - записывать текст "безопасным способом".
+// С помощью textContent можем получить только текст.
+/*
+textElement.textContent = `<p>${textElementContent}</p> <p>And 
     <span class="yellow">React</span> and Typescript</p>`;
 
-// console.log(textElement.outerHTML);
+console.log(textElement.textContent);
+*/
+/*
+Мы не хотим, чтобы на сайте появлялся произвольный ХТМЛ-код.
+Присваивание через textContent - один из способов от этого 
+защититься.
+*/
