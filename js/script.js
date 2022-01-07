1b7d8e10d53b25d00812c1a26a195ea13b4ec918
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-01-07 23:41:42 +0200

    Вставка нескольких фрагментов сразу

--- js/script.js
+++ js/script.js
@@ -13,31 +13,16 @@ const newElement = document.createElement('div');
 // Наполняем новый элемент
 newElement.innerHTML = `Learn JS <span class="yellow">React</span>!`;
 
-// Вставляем новый элемент... 
+// Вставка нескольких фрагментов сразу
+textElement.append(newElement, "Hello!");
 
-// ...перед обьектом
-// textElement.before(newElement);
-// ...после обьекта
-textElement.after(newElement);
-// ...внутрь и в начало обьекта
-// textElement.prepend(newElement);
-// ...внутрь и в конец обьекта
-//textElement.append(newElement);
 /*
-Синтаксис записи прост - обращаемся к обьекту с которым хотим взаимодействовать, пишем 
-тот или иной метод, в зависимости от того куда мы хотим вставить элемент и в круглых скобках 
-мы пишем наш элемент.
-
-Вот более наглядная схема:
+// Можно вставлять строку
+textElement.append(`Learn JS <span class="yellow">React</span>!`);
 */
-// lesson__list - 
-
-// before
-<ul class="lesson__list">
-   //prepend    
-    <li>Пункт 1</li>
-    <li>Пункт 2</li>
-    <li>Пункт 3</li>
-   // append
-</ul>
-// after
+/*
+Другими словами, строки вставляются безопасным способом, как это 
+делает textContent. Поэтому эти методы могут использоваться только 
+для вставки ДОМ-узлов или текстовых фрагментов. А чтоЮ если мы хотим 
+вставить ХТМЛ именно "как ХТМЛ", со всемитегами и прочим, как это делает innerHTML?
+*/
