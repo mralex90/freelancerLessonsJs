e21babb016996ccd1d98f838b557ec870760f058
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-01-10 22:27:08 +0200

    insertAdjacentHTML

--- js/script.js
+++ js/script.js
@@ -3,26 +3,21 @@
 // Изменение документа
 
 // Mетоды вставки
+// insertAdjacentHTML/Text/Element
 
 // Получаем обьект
 const textElement = document.querySelector('.lesson__text');
 
-// Создание нового элемента (тега)
-const newElement = document.createElement('div');
+// Вставляем текст, HTML, элемент
 
-// Наполняем новый элемент
-newElement.innerHTML = `Learn JS <span class="yellow">React</span>!`;
-
-// Вставка нескольких фрагментов сразу
-textElement.append(newElement, "Hello!");
+textElement.insertAdjacentHTML( // есть 2 параметра
+    'afterend',  // 1й параметр отвечает куда именно будет вставлен 2й параметр
+    `<p>Learn JS <span class="yellow">React</span>!</p>`// 2й параметр и есть наша строка, внутрь которой можем писать разные теги/строки и тд
+);
 
 /*
-// Можно вставлять строку
-textElement.append(`Learn JS <span class="yellow">React</span>!`);
-*/
-/*
-Другими словами, строки вставляются безопасным способом, как это 
-делает textContent. Поэтому эти методы могут использоваться только 
-для вставки ДОМ-узлов или текстовых фрагментов. А чтоЮ если мы хотим 
-вставить ХТМЛ именно "как ХТМЛ", со всемитегами и прочим, как это делает innerHTML?
+"beforebegin" - вставить html непосредственно перед textElement
+"afterbegin" - вставить html в начало textElement
+"beforeend" - вставитть html в конец textElement
+"afterend" - вставить html непосредственно после textElement
 */
