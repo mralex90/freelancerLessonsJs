f926e4b096ce8e5378ec23ab17489107e2de09a8
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-01-10 22:54:28 +0200

    Методы вставки insertAdjacentHTML/Text/Element

--- js/script.js
+++ js/script.js
@@ -1,32 +1,13 @@
 
-// DOM
-// Изменение документа
+// Методы вставки insertAdjacentHTML/Text/Element
+// lesson__list - это наш обьект 
 
-// Mетоды вставки
-// Дополнительно существуют insertAdjacentText и insertAdjacentElement
-
-// Получаем обьект
-const textElement = document.querySelector('.lesson__text');
-
-// Вставляем текст
-textElement.insertAdjacentText( // есть 2 параметра
-    'beforeend',  // 1й параметр отвечает куда именно будет вставлен 2й параметр
-    `<p>Learn JS <span class="yellow">React</span>!</p>`// 2й параметр и есть наша строка, внутрь которой можем писать разные теги/строки и тд
-)
-// Создание нового элемента (тега)
-const newElement = document.createElement('div');
-// Наполняем новый элемент
-newElement.innerHTML = `Learn JS <span class="yellow">React</span>!`;
-
-// Вставляем элемент
-textElement.insertAdjacentText(
-    'beforeend',
-    newElement
-)
-
-/*
-На практике часто используется только "insertAdjacentHTML".
-Потому что для элементов и текста у нас есть методы 
-append/prepend/before/after - их быстрее написать, и они 
-могут вставлять как узлы, так и текст.
-*/
+// beforebegin  --- вставить до нашего обьекта
+<ul class="lesson__list">
+    {/* afterbegin --- вставить внутрь обьекта перед содержимым */}
+    <li>Point #1</li>
+    <li>Point #2</li>
+    <li>Point #3</li>
+    {/* beforeend  --- вставить внуть обьекта после содержимого */
+</ul>
+// afterend --- втавить после нашего обьекта
