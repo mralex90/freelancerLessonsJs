e7b3aba0c1b802b5763bfbf78bb8a490e5f87685
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-01-10 22:44:24 +0200

    insertAdjacentText/insertAdjacentElement

--- js/script.js
+++ js/script.js
@@ -3,21 +3,30 @@
 // Изменение документа
 
 // Mетоды вставки
-// insertAdjacentHTML/Text/Element
+// Дополнительно существуют insertAdjacentText и insertAdjacentElement
 
 // Получаем обьект
 const textElement = document.querySelector('.lesson__text');
 
-// Вставляем текст, HTML, элемент
-
-textElement.insertAdjacentHTML( // есть 2 параметра
-    'afterend',  // 1й параметр отвечает куда именно будет вставлен 2й параметр
+// Вставляем текст
+textElement.insertAdjacentText( // есть 2 параметра
+    'beforeend',  // 1й параметр отвечает куда именно будет вставлен 2й параметр
     `<p>Learn JS <span class="yellow">React</span>!</p>`// 2й параметр и есть наша строка, внутрь которой можем писать разные теги/строки и тд
-);
+)
+// Создание нового элемента (тега)
+const newElement = document.createElement('div');
+// Наполняем новый элемент
+newElement.innerHTML = `Learn JS <span class="yellow">React</span>!`;
+
+// Вставляем элемент
+textElement.insertAdjacentText(
+    'beforeend',
+    newElement
+)
 
 /*
-"beforebegin" - вставить html непосредственно перед textElement
-"afterbegin" - вставить html в начало textElement
-"beforeend" - вставитть html в конец textElement
-"afterend" - вставить html непосредственно после textElement
+На практике часто используется только "insertAdjacentHTML".
+Потому что для элементов и текста у нас есть методы 
+append/prepend/before/after - их быстрее написать, и они 
+могут вставлять как узлы, так и текст.
 */
