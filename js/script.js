193e0072ce007e5eace9f07532fd28c81cb5aed0
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-01-25 22:17:26 +0200

    scrollWidth  и scrollHeight

--- js/script.js
+++ js/script.js
@@ -4,24 +4,23 @@
 // Получаем обьект
 const block = document.querySelector('.lesson__block');
 
-// Размеры обьекта без раиок и полосы прокрутки
-// clientWidth & clientHeight
+// Размеры обьекта включающие в себя прокрученную (которую не видно) часть.
+// В остальном работают как clientWidth/clientHeight, scrollWidth u scrollHeight
 
-const elementClientWidth = block.clientWidth;
-const elementClientHeight = block.clientHeight;
+const elementScrollWidth = block.scrollWidth;
+const elementScrollHeight = block.scrollHeight;
 
-console.log(elementClientWidth);
-console.log(elementClientHeight);
+console.log(elementScrollWidth);
+console.log(elementScrollHeight);
 
 /*
-clientWidth & clientHeight - это тоже размеры обьекта, его ширина и высота, 
-но без рамок и без полосы прокрутки.
-Рассмотрим на примере: 
-Создаем две константы, обращаемся к нашему блоку, получаем clientWidth и 
-clientHeight, выводим в консоль.
-Видим, что ширина 443рх, высота 260рх. Разберемся из чего же состоит данная ширина.
-Общая рамка (offsetWidth) - рамка слева - рамка справа - ширина скролла
- 500 - 20 - 20 - 17 = 443 
+Опять поговорим о ширине и высоте нашего обьекта, но свойства scrollWidth 
+и scrollHeight уже будут включать в себя и ту часть, которую мы не видим за скроллом.
+Контент у нас довольно большой, появляется скролл и вот эти свойства вернут полный 
+размер. В остальном они работают точьно так же как и clientWidth и clientHeight, 
+т.е. не включают в себя ни рамки ни ширину скролла.
 
+Создаем константы, получаем свойства, выводим в консоль. И ширина у нас все та же (443рх), 
+так как у нас нет горизонтального скролла, а вот высота уже 470рх (высота всего контента в этом блоке).
 
 */
