81157daa4479230a1cdb6f254beb95074fb18f9d
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-05 21:33:34 +0200

    event.target / event.relatedTarget

--- js/script.js
+++ js/script.js
@@ -4,21 +4,29 @@
 // Наведение мыши: mouseover/out, mouseenter/leave
 
 // События mouseover/mouseout, relatedTarget
+
 /*
-Событие "mouseover" происходит в момент, когда курсор оказывается нал элементом, 
-а событие mouseout - в момент, когда курсор уходит с элемента.
+Эти события являются особенными, потому что у них имеется свойство "relatedTarget". 
+Оно "дополняет" Target. Когда мышь переходит с одного элемента на другой, то один 
+из них будет Target, а другой relatedTarget.
+
+Для события mouseover:
+event.target - это элемент, на который курсор перешел.
+event.relatedTarget - это элемент, с которого курсор ушел (relatedTarget -> target).
+
+Для события mouseout наоборот: 
+event.target - это элемент, с которого курсор ушел.
+event.relatedTarget - это элемент, на который курсор перешел (target -> relatedTarget)
 */
 
 const blockForMouse = document.querySelector('.block-for-mouse');
 
 blockForMouse.addEventListener("mouseover", function (event) {
-    blockForMouse.innerHTML = `Курсор над элементом`;
+    console.log(event.target);
+    console.log(event.relatedTarget);
 });
 
 blockForMouse.addEventListener("mouseout", function (event) {
-    blockForMouse.innerHTML = `Курсор уходит с элемента`;
+    console.log(event.target);
+    console.log(event.relatedTarget);
 });
-/*
-Событие "mouseover" происходит в момент, когда курсор оказывается над обьектом, а 
-"mouseout" - когда курсор уходит с этого обьекта.
-*/
