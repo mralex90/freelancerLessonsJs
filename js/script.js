3084f577b18ec9e08b0cd582e53f77222c2876e1
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-05 21:45:27 +0200

    События mouseenter и mouseleave

--- js/script.js
+++ js/script.js
@@ -3,30 +3,29 @@
 
 // Наведение мыши: mouseover/out, mouseenter/leave
 
-// События mouseover/mouseout, relatedTarget
+// События mouseenter и mouseleave
 
 /*
-Эти события являются особенными, потому что у них имеется свойство "relatedTarget". 
-Оно "дополняет" Target. Когда мышь переходит с одного элемента на другой, то один 
-из них будет Target, а другой relatedTarget.
+На первый взгляд пара событий mouseenter / mouseleave ничем не отличаются 
+от mouseover и mouseout. То же самое - наводим на обьект, выполняется код, 
+уходим с обьекта - выполняется другой код. НО! 
+Пара важных отличий:
+1) Переходы внутри элемента, на его потомки и с них, не считаются.
+2) События mouseenter / mouseleave не всплывают.
 
-Для события mouseover:
-event.target - это элемент, на который курсор перешел.
-event.relatedTarget - это элемент, с которого курсор ушел (relatedTarget -> target).
-
-Для события mouseout наоборот: 
-event.target - это элемент, с которого курсор ушел.
-event.relatedTarget - это элемент, на который курсор перешел (target -> relatedTarget)
+И тут вроде видим преимущество, так как мы снижаем нагрузку на браузер, у нас 
+нет череды повторяющихся событий, нам просто нужно навести на обьект, выполнить 
+какие-то действия, когда мы уходим с обьекта - выпоолнить другие действия. А не 
+выполнять все постоянно, когда мы внутри обьекта перемещаемся между его потомками 
+и т.д.
 */
 
 const blockForMouse = document.querySelector('.block-for-mouse');
 
-blockForMouse.addEventListener("mouseover", function (event) {
-    console.log(event.target);
-    console.log(event.relatedTarget);
+blockForMouse.addEventListener("mouseenter", function (event) {
+    console.log(`Курсор над элементом`);
 });
 
-blockForMouse.addEventListener("mouseout", function (event) {
-    console.log(event.target);
-    console.log(event.relatedTarget);
+blockForMouse.addEventListener("mouseleave", function (event) {
+    console.log(`Курсор уходит с элемента`);  
 });
