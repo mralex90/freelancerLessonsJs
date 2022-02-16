cc207bd59ebde36b71a4860dba078c516f91e326
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-16 23:14:49 +0200

    select & option

--- js/script.js
+++ js/script.js
@@ -1,47 +1,38 @@
 
-// Элементы форм
+// Элементы форм 
 
-// Работа с input и textarea
-
+// Работа с select и option
 /*
-Значение можно получить через свойство input.value(если это строка) 
-или input.checked(если это булево значение) для чекбоксов и радиокнопок.
-*/
-const mainForm = document.forms.main;
+Элемент <select> имеет 3 важных свойства:
+select.options - коллекция из подэлементов <option>,
+select.value - значение выбраного в данный момент <option>,
+select.selectedIndex - номер выбранного <option>.
 
-const mainFormRadioButtons = mainForm.nameRadio;
-const mainFormCheckBox = mainForm.nameCheck;
-const mainFormFile = mainForm.nameFile;
+Они дают три разных способа устранить значение в <select>:
+Найти соответствующий элемент <option> и установить в option.selected 
+значение true.
+Установить в select.value значение нужного <option>.
+Установить в select.selectedIndex номер нужного <option>.
 
-// Получить значение поля type radio
-mainFormRadioButtons[0].value = "left";
-mainFormRadioButtons[1].value = "right";
-mainFormRadioButtons[0].checked = true;
-
-// Назначить значение поля type checkbox
-/*
-mainFormCheckBox.value = "save";
-mainFormCheckBox.checked = true;
+Первый способ наиболее понятный, но (2) и (3) являются более удобными в работе.
 */
 
-// Назначить значение поля type file
-//mainFormFile.value = "";
+const mainForm =  document.forms.main;
+const mainFormSelect = mainForm.nameSelect;
+
+// Получить все options
+console.log(mainFormSelect.options);
+
+// Получить индекс выбранного option
+const mainFormSelectIndex = mainFormSelect.selectedIndex;
+console.log(mainFormSelectIndex);
+// Получить значение выбранного option
+const mainFormSelectValue = mainFormSelect.value;
+console.log(mainFormSelectValue);
+// Получить текст выбранного option 
+const mainFormSelectText = mainFormSelect.options[mainFormSelectIndex].text;
+console.log(mainFormSelectText);
 
 /*
-Теперь научимся  присваивать значения этим эдементам из ДЖС.
-Начнем с радиоКнопок.
-Для того,чтобы это сделать нужно сделать следующее: обращаемся к той или 
-иной кнопке, пишем value, присваиваем новое значение этого атрибута. А для того, 
-чтобы выбрать ту или иную кнопку - мы используем "checked".
-*/
 
-/*
-То же самое мы делаем с чекбоксом - с помощью value мы задаем свое значение этого 
-атрибута, а с помощью checked = true/false мы управляем выбран этоти обьект или нет.
 */
-/*
-Другая ситуация складывается с типом "file", так как мы не можем задать значение 
-этого поля вот таким образом.
-Все что мы можем сделать - это очистить хначение, т.е. передать пустую строку.
-*/
-
