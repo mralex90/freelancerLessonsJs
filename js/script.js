5a0c2f0b68f860717607a809ae11e39e32f746b2
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-16 23:31:03 +0200

    Работа с select и option

--- js/script.js
+++ js/script.js
@@ -17,22 +17,33 @@ select.selectedIndex - номер выбранного <option>.
 Первый способ наиболее понятный, но (2) и (3) являются более удобными в работе.
 */
 
-const mainForm =  document.forms.main;
-const mainFormSelect = mainForm.nameSelect;
+const mainForm = document.forms.main;
+const mainFormSelect = mainForm.nameSelect; /* в эту константу получаем элемент нашего списка */
 
-// Получить все options
+// Чтобы Получить все options - обращаемся к списку, ставим точку и пишем options
+// При выводе в консоль получим коллекцию наших обьектов
 console.log(mainFormSelect.options);
 
-// Получить индекс выбранного option
+/*  Получить индекс выбранного option
+Используем selectedIndex, в итоге получаем 0, т.е. нулевой индекс - первый индекс в ХТМЛ у нас 
+выбран, поэтому мы получаем это значение.
+*/
 const mainFormSelectIndex = mainFormSelect.selectedIndex;
 console.log(mainFormSelectIndex);
-// Получить значение выбранного option
+
+/* Получить значение выбранного option,
+т.е. значение атрибута value, мы используем свойство value и получаем 1. Именно это указано в примере в ХТМЛ.
+*/
 const mainFormSelectValue = mainFormSelect.value;
 console.log(mainFormSelectValue);
-// Получить текст выбранного option 
+
+/* Получить текст выбранного option, 
+тогда обращаемся к нашему select, пишем options, но, в квадратных скобках указываем индекс выбранного 
+option. Далее ставим точку и пишем text. При выводе в консоль и получим 20, а именно текст выбранного option
+*/
 const mainFormSelectText = mainFormSelect.options[mainFormSelectIndex].text;
 console.log(mainFormSelectText);
 
 /*
-
+11/37
 */
