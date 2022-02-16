b588f1f6a81ae0a841ab9ff27701f4877481b7eb
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-16 22:57:45 +0200

    РадиоКнопки, Чекбоксы, тип file

--- js/script.js
+++ js/script.js
@@ -13,26 +13,35 @@ const mainFormRadioButtons = mainForm.nameRadio;
 const mainFormCheckBox = mainForm.nameCheck;
 const mainFormFile = mainForm.nameFile;
 
-// Получить значение поляtype radio
+// Получить значение поля type radio
+mainFormRadioButtons[0].value = "left";
+mainFormRadioButtons[1].value = "right";
+mainFormRadioButtons[0].checked = true;
+
+// Назначить значение поля type checkbox
 /*
-console.log(mainFormRadioButtons[0].value);
-console.log(mainFormRadioButtons[1].value);
-console.log(mainFormRadioButtons[0].checked);
-console.log(mainFormRadioButtons[1].checked);
+mainFormCheckBox.value = "save";
+mainFormCheckBox.checked = true;
 */
 
-// Получить значение поля type checkbox
+// Назначить значение поля type file
+//mainFormFile.value = "";
+
 /*
-console.log(mainFormCheckBox.value);
-console.log(mainFormCheckBox.checked);
+Теперь научимся  присваивать значения этим эдементам из ДЖС.
+Начнем с радиоКнопок.
+Для того,чтобы это сделать нужно сделать следующее: обращаемся к той или 
+иной кнопке, пишем value, присваиваем новое значение этого атрибута. А для того, 
+чтобы выбрать ту или иную кнопку - мы используем "checked".
 */
 
-// Получить значение поля type file
-console.log(mainFormFile.value);
-//////////////////////////////////////////////////////////////////////
 /*
-Input с типом file.
-Это такая штука, где мы можем выбрать например какую-то картинку или любой другой 
-файл. Здесь можем получить значение с помощью "value", в данный момент оно пустое, 
-никакой файл не выьран.
+То же самое мы делаем с чекбоксом - с помощью value мы задаем свое значение этого 
+атрибута, а с помощью checked = true/false мы управляем выбран этоти обьект или нет.
 */
+/*
+Другая ситуация складывается с типом "file", так как мы не можем задать значение 
+этого поля вот таким образом.
+Все что мы можем сделать - это очистить хначение, т.е. передать пустую строку.
+*/
+
