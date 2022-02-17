7f2383c27202e214dee20545c61bcc10b8d082f9
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-17 21:07:23 +0200

    Элемент option

--- js/script.js
+++ js/script.js
@@ -3,47 +3,13 @@
 
 // Работа с select и option
 /*
-Элемент <select> имеет 3 важных свойства:
-select.options - коллекция из подэлементов <option>,
-select.value - значение выбраного в данный момент <option>,
-select.selectedIndex - номер выбранного <option>.
+Элемент option имеет следуещие свойства:
 
-Они дают три разных способа устранить значение в <select>:
-Найти соответствующий элемент <option> и установить в option.selected 
-значение true.
-Установить в select.value значение нужного <option>.
-Установить в select.selectedIndex номер нужного <option>.
+option.selected - вернет true или false и будет означать выбран этот 
+    option или нет 
 
-Первый способ наиболее понятный, но (2) и (3) являются более удобными в работе.
-*/
+option.index - это номер среди других option в данном select'е. 
 
-const mainForm = document.forms.main;
-const mainFormSelect = mainForm.nameSelect; /* в эту константу получаем элемент нашего списка */
-
-// Чтобы Получить все options - обращаемся к списку, ставим точку и пишем options
-// При выводе в консоль получим коллекцию наших обьектов
-console.log(mainFormSelect.options);
-
-/*  Получить индекс выбранного option
-Используем selectedIndex, в итоге получаем 0, т.е. нулевой индекс - первый индекс в ХТМЛ у нас 
-выбран, поэтому мы получаем это значение.
-*/
-const mainFormSelectIndex = mainFormSelect.selectedIndex;
-console.log(mainFormSelectIndex);
-
-/* Получить значение выбранного option,
-т.е. значение атрибута value, мы используем свойство value и получаем 1. Именно это указано в примере в ХТМЛ.
-*/
-const mainFormSelectValue = mainFormSelect.value;
-console.log(mainFormSelectValue);
-
-/* Получить текст выбранного option, 
-тогда обращаемся к нашему select, пишем options, но, в квадратных скобках указываем индекс выбранного 
-option. Далее ставим точку и пишем text. При выводе в консоль и получим 20, а именно текст выбранного option
-*/
-const mainFormSelectText = mainFormSelect.options[mainFormSelectIndex].text;
-console.log(mainFormSelectText);
-
-/*
-11/37
+option.text - и собственно, текст содержимого опции. текст, который 
+    виден пользователю.     
 */
