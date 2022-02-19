543b118ebb78f84ee011387cb4e20385edb34c90
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-19 19:29:11 +0200

    Методы elem.focus() и elem.blur()

--- js/script.js
+++ js/script.js
@@ -1,41 +1,20 @@
 
 // События форм и их элементов
 
-// События focus и blur
+// Методы elem.focus() и elem.blur() устанавливают или снимают фокус.
 
 const mainForm = document.forms.main;
 const mainFormInput = mainForm.nameInput;
 
-const mainFormInputPlaceholder = mainFormInput.placeholder;
+mainFormInput.focus();
 
-mainFormInput.addEventListener("focus", function (e) {
-    mainFormInput.placeholder = "";
-});
-mainFormInput.addEventListener("blur", function (e) {
-    mainFormInput.placeholder = mainFormInputPlaceholder;
-});
-
-// Действия могут быть самыми разными: валидация поля, отправка формы и т.д.
+setTimeout(() => {
+    mainFormInput.blur();
+}, 3000);
 /*
-Пример:
-В константы получаем саму форму, поле ввода с именем "nameInput", а также в 
-отдельную константу "mainFormInputPlaceholder" получаем значение атрибута 
-"placeholder" этого поля ввода.
-В placeholder сейчас написано "Введите что-то...". Так вот, нам нужно чтобы когда 
-поле получало фокус, этот placeholder был равен пустой строке, т.е. исчезал.
-А в момент потери фокуса, если поле не заполнено, то мы опять увидим placeholder, 
-а именно "ВВедите что-то...".
-Это работает так:
-Мы кликаем на элемент (он получает фокус) - placeholder исчез, и теперь мы можем 
-что-то вводить. Момент потери фокуса мы увидим то что мы ввели, т.к. placeholder 
-в данный момент скрыт. Но, если мы оставим пустую строку и потеряем фокус - мы увидим 
-placeholder.
-
-Выполняется это с помощью простых манипуляций. В момент получения фокуса, т.е. по событию 
-"focus" мы передаем значение для атрибута placeholder пустую строку таким образом 
-мы добиваемся того, что placeholder исчезает (mainFormInput.placeholder = "";).
-Но, в момент потери фокуса, поскольку мы заранее получили в константу значение "placeholder", 
-мы возвращаем это значение в атрибут (mainFormInput.placeholder = mainFormInputPlaceholder).
-И если поле пустое в момент потери фокуса, мы увидим значение "placeholder".
-
-*/ 
+Кроме событий фокустровки существуют еще и методы focus / blur. 
+С их помощью мы можем управлять из ДЖС фокусировкой того или иного 
+элемента. Например следующий код после загрузки страницы автоматически 
+добавит фокус на элемент с именем "nameInput", а спустя 3 секунды 
+фокус исчезнет.
+*/
